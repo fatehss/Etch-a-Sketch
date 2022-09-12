c3af56658d0f0b857522e5157da41b1db736be95
fatehss <fatehss@g.ucla.edu>
2022-09-12 13:44:56 -0700

    finish first complete version of Etch a sketch
    Implement grid lines, toggle painting, variable grid size, and simple css

--- script.js
+++ script.js
@@ -2,20 +2,62 @@
 
 const grid_height = 400;
 const grid_width = 400;
-const num_cols = 50;   //dimensions of the grid
-const num_rows = 50;
+let num_cols = 50;   //dimensions of the grid
+let num_rows = 50;
 
 const container = document.querySelector('.sketch-container');
-const grid = [];
-let color = 'green';
+
+container.style.height = grid_height;
+container.style.height = grid_width;
+
+let color = 'black';    //stores color
+const colorPicker = document.querySelector('#pick-color');
+colorPicker.addEventListener('input', function(e){
+    color = e.target.value;
+})
+
+const gridDimensions = document.querySelector('#range-slider');
+gridDimensions.addEventListener('input', function(e){
+    num_cols = e.target.value;
+    num_rows = e.target.value;
+    console.log('working');
+    container.replaceChildren(); //removes all child nodes and their child nodes
+    setGrid(num_cols, num_rows);
+});
+
+
+function toggleGridLines(){
+    const boxes = document.querySelectorAll('.box');
+    for (let i =0; i< boxes.length; ++i){
+        if (boxes[i].style.border != '1px solid black'){
+            boxes[i].style.border = '1px solid black';
+            boxes[i].style.margin = '-1px';
+        }
+        else{
+            boxes[i].style.margin = '0px';
+            boxes[i].style.border = 'none';
+        }
+
+  
+    }
+}
+const gridLines = document.querySelector('.grid-lines-box');
+gridLines.addEventListener('change',toggleGridLines);
+
+let pause = false;
+document.addEventListener('keypress',(e) => {
+   if (e.key === 'Shift') {pause = (!pause)};
+});
 
 function setGrid(num_rows, num_cols)
 {
+    gridLines.checked = '';
     for(let i =0; i<num_rows; ++i)
     { //initialize using a standard nested for loop, creating rows then indivual boxes
 
         let newRow = document.createElement('div'); //
         newRow.style.display = 'flex';
+        newRow.style.flex = '1 1 auto'
         newRow.style.height = (grid_height/num_rows) + 'px';
         newRow.style.width = grid_width + 'px';
         for (let j = 0; j<num_cols; ++j){
@@ -23,11 +65,9 @@ function setGrid(num_rows, num_cols)
             newBox.style.height = (grid_height/num_rows) + 'px';
             newBox.style.width = (grid_width/num_rows) + 'px';
             newBox.classList.add('box');
-            if (j%2 == 0 && i%2 == 0){(newBox.style.backgroundColor = 'blue');}
-            else if (j%2 == 1 && i%2 == 1){(newBox.style.backgroundColor = 'red')}
 
             newBox.addEventListener('mouseover', function(e){   //function that sets the sketch value to a
-                e.target.style.backgroundColor = color;
+                if (!pause){e.target.style.backgroundColor = color;}
             })
             newRow.appendChild(newBox);
         }
@@ -51,14 +91,6 @@ resetButton.onclick = function ()
 
 
 
-
-
-/*
-How to create a 16x16 grid of divs in js?
-
-I need to create a 2d array. I can first create 16 rows and append them to the container, then worry about the columns
-
-**/
 
 
 
